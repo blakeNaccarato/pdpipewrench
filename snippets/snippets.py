8039be3009df7addcdcbcb866f7682eabef96b4d
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-02-27 08:08:50 -0800

    started snippets folder for unused code fragments

---
+++ snippets/snippets.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+# pylint: disable-all
+# type: ignore
+
+#? Originally in pdpipewrench/__init__.py as a check on source and sink filenames.
+#? Removed after implementing source/sink classes.
+# get paths from config.yaml, either relative to config.yaml dir or absolute
+data_template = {"data": {"source": confuse.Filename(), "sink": confuse.Filename()}}
+# if key is missing and not in pdpipewrench/config_default.yaml, raise a descriptive
+# error based on the original, less descriptive confuse.NotFoundError
+try:
+    data_files = config.get(data_template)
+except confuse.NotFoundError as exc:
+    msg = exc.args[0]
+    missing_key = msg.split()[0].replace(".", ": ")
+    msg = f"Missing <value> for '{missing_key}: <value>' in \n{config_filepath}"
+    raise confuse.NotFoundError(msg)
