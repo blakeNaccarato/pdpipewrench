a6817f4b4d57e7a93a2834e47df6cd755ab1666f
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-03-04 09:16:21 -0800

    "git flow" and pythonpublish.yml don't play nicely i will just publish manually

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdpipewrench",
-    version="0.10",
+    version="1.0",
     url="https://github.com/blakeNaccarato/pdpipewrench",
     author="Blake Naccarato",
     package_dir={"": "src"},
