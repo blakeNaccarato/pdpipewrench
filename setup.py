682a4d9f7fdfc70c0852feb13dcbd93cc90b3b06
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-03-04 08:39:50 -0800

    version bump, hopefully workflow will finally work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdpipewrench",
-    version="0.7",
+    version="0.8",
     url="https://github.com/blakeNaccarato/pdpipewrench",
     author="Blake Naccarato",
     package_dir={"": "src"},
