26f944fd2fe2f2ca47334ae6e0227a731475eac4
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-03-06 14:43:37 -0800

    implement df_copy() decorator (fixes #1)

--- example/example.py
+++ example/example.py
@@ -1,5 +1,6 @@
 import custom_functions
 import pdpipewrench as pdpw
+from IPython.display import display
 
 src = pdpw.Source("example_source")  # generate the source from `config.yaml`
 snk = pdpw.Sink("example_sink")  # generate the sink from `config.yaml`.
@@ -8,7 +9,9 @@ snk = pdpw.Sink("example_sink")  # generate the sink from `config.yaml`.
 line = pdpw.Line("example_pipeline", custom_functions)
 
 # connect the source and sink to the pipeline, print what the pipeline will do, then run
-# the pipeline, writing the output to disk. capture the input/output dataframes if desired.
+# the pipeline, writing the output to disk. capture the input/output dataframes if
+# desired.
 pipeline = line.connect(src, snk)
 print(pipeline)
-(dfs_in, dfs_out) = line.run()
+dfs = line.run()
+display(dfs)
