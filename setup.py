e1d8f646aa42e770550f6f6a7b70baa0ed1924a2
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-03-06 15:20:08 -0800

    add `setuptools` to [dev] `pip install -U -e .[dev]` will now prepare the venv for building

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     extras_require={  # pip install -e .[dev]
         "dev": [
             # build
+            "setuptools",
             "wheel",
             "twine",
             # data science
