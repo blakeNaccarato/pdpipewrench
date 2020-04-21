cfe57f39c422065261fef99444ac591a0c2d13c6
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-04-21 15:41:31 -0700

    change example output to display side-by-side

--- example/example.py
+++ example/example.py
@@ -1,6 +1,6 @@
 import custom_functions
 import pdpipewrench as pdpw
-from IPython.display import display
+from IPython.display import display_html
 
 src = pdpw.Source("example_source")  # generate the source from `config.yaml`
 snk = pdpw.Sink("example_sink")  # generate the sink from `config.yaml`.
@@ -11,7 +11,10 @@ line = pdpw.Line("example_pipeline", custom_functions)
 # connect the source and sink to the pipeline, print what the pipeline will do, then run
 # the pipeline, writing the output to disk. capture the input/output dataframes if
 # desired.
-pipeline = line.connect(src, snk)
-print(pipeline)
-dfs = line.run()
-display(dfs)
+dfs_in = line.connect(src, snk)
+print(line.pipeline)
+dfs_out = line.run()
+for df_in, df_out in zip(dfs_in, dfs_out):
+    html = df_in.to_html() + "  --->  " + df_out.to_html()
+    html = html.replace("table", "table style='display:inline'")
+    display_html(html, raw=True)
