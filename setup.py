29f96020fc2b9af678ec55a811b5b3ceec1ce44c
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-04-22 10:23:15 -0700

    apply a proper version. version had climbed to 1.1.1 while trying to figure out publishing workflows.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="pdpipewrench",
-    version="1.1.1",
+    version="0.1.0",
     description="YAML-configurable Pandas pipelines.",
     long_description=long_description,
     long_description_content_type='text/markdown',
